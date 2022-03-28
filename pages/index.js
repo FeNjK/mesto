251709a1ce946f600581232169edfa19af8aac73
FeNjK <bodhisatva_xp@mail.ru>
2022-03-28 20:27:03 +0300

    Пилю проект как могу, голова не работает.

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+let button_like = document.querySelector('.element__mark');
+
+
+console.log(button_like);
+
+
+//function myFunction(like){
+  elem.classList.toggle('clicked');
+    if(elem === btn_up){
+      btn_down.classList.remove('clicked');
+    } else {
+      btn_up.classList.remove('clicked');
+    }//
+
+
+
+    let merry = true;
