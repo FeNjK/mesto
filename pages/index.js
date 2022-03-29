347f21bd78876ffd612accf06f6eb2644be532b8
FeNjK <bodhisatva_xp@mail.ru>
2022-03-29 12:47:48 +0300

    Создал лайк по фокусу. Перехожу к popup

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-let button_like = document.querySelector('.element__mark');
+//let button_like = document.querySelector('.element__mark');
 
 
 
 //function myFunction(like){
-  elem.classList.toggle('clicked');
-    if(elem === btn_up){
-      btn_down.classList.remove('clicked');
-    } else {
-      btn_up.classList.remove('clicked');
-    }//
+//  elem.classList.toggle('clicked');
+//    if(elem === button_like){
+//      button-like.classList.remove('clicked');
+//    } else {
+//      button-like.classList.remove('clicked');
+//    }
+//  }
+
+//  myFunction(like)
+
+//console.log(myFunction)
+
+
+//document.querySelector("#submit").onclick = function(){
+//  alert("Вы нажали на кнопку");
+//}
+
+
+
+//Функция лайка по клику на кнопку//
+
+
+let btn_like = document.querySelector('.button__mark');
+
+console.log(btn_like);
+
