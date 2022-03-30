0556227a7f743c808d7657f0e669283af51ea35b
FeNjK <bodhisatva_xp@mail.ru>
2022-03-30 22:56:18 +0300

    Подготовка к проверке

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
-// Реализация функции открытия/закрытия модального окна
-// кнопки редактирования информации о пользователе
+// Реализация функции открытия/закрытия модального окна кнопки редактирования информации о пользователе
 
 const editingButton = document.querySelector('.profile-info__editing-button'); //Нашли кнопку
 const modalWindow = document.querySelector('.popup'); //нашли модальное окно
@@ -24,20 +23,20 @@ modalWindow.addEventListener('click', onOverlayClick); //добавили про
 //Реализация функции редактирования полей профиля
 
 const formUserData = document.querySelector('.profile-info__data');//нашли форму с данными профиля пользователя
-const userName = document.querySelector('.profile-info__name');//нашли поле с именем подбзователя
-const userActivityType = document.querySelector('.profile-info__activity-type');// нашли поле с родом деятельности пльзователя
+const userName = document.querySelector('.profile-info__name');//нашли поле с именем пользователя
+const userActivityType = document.querySelector('.profile-info__activity-type');// нашли поле с родом деятельности пользователя
 
 const popUpFormUserData = document.querySelector('.popup__form');//в модальном окне нашли форму с данными профиля пользователя
-const popUpUserName = document.querySelector('.popup__input-name');//в модальном окне нашли поле с именем подбзователя
-const popUpUserActivityType = document.querySelector('.popup__input-activity-type');//в модальном окне нашли поле с родом деятельности пльзователя
+const popUpUserName = document.querySelector('.popup__input-name');//в модальном окне нашли поле с именем пользователя
+const popUpUserActivityType = document.querySelector('.popup__input-activity-type');//в модальном окне нашли поле с родом деятельности пользователя
 
 function formSubmitHandler (event) {
   event.preventDefault();// Эта строчка отменяет стандартную отправку формы.
 
-  popUpUserName.textContent = userName.value; //ввод текста в поле Name модального окна меняет текст с именем в профиле
-  popUpUserActivityType.textContent = userActivityType.value;//ввод текста в поле с родом деятельности модального окна меняет срдержимое соответствующего поля в профиле
+  userName.textContent = popUpUserName.value; //ввод текста в поле Name модального окна меняет текст с именем в профиле
+  userActivityType.textContent = popUpUserActivityType.value;//ввод текста в поле с родом деятельности модального окна меняет срдержимое соответствующего поля в профиле
   
   toggleEditWindow(); //после выполнения функции редактирования данных профиля происходит закрытие модального окна
 }
 
-popUpFormUserData.addEventListener('submit', formSubmitHandler);
+popUpFormUserData.addEventListener('submit', formSubmitHandler); //Обработчик события, отвечающий за отправку данных профиля
