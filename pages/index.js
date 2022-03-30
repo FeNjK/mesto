a08946e8419352cf9ca66e96e4482c02a31bc3b7
FeNjK <bodhisatva_xp@mail.ru>
2022-03-30 16:07:47 +0300

    Закончил с версткой, приступаю к JS

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,11 @@
-//let button_like = document.querySelector('.element__mark');
+// Сначала ищем кнопку редактирования профиля
+
+const profile = document.querySelector('.profile');
+const profileInfo = profile.querySelector('.profile-info');
+const editButton = profileInfo.querySelector('.profile-info__edit-button');
 
 
+// Реализация функции открытия/закрытия модального окна
+// кнопки редактирования информации о пользователе
 
-//function myFunction(like){
-//  elem.classList.toggle('clicked');
-//    if(elem === button_like){
-//      button-like.classList.remove('clicked');
-//    } else {
-//      button-like.classList.remove('clicked');
-//    }
-//  }
-
-//  myFunction(like)
-
-//console.log(myFunction)
-
-
-//document.querySelector("#submit").onclick = function(){
-//  alert("Вы нажали на кнопку");
-//}
-
-
-
-//Функция лайка по клику на кнопку//
-
-
-let btn_like = document.querySelector('.button__mark');
-
-console.log(btn_like);
 
