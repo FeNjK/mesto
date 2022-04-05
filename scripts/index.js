e269432a0aa2ed76e238b663d4096196efe25b8e
FeNjK <bodhisatva_xp@mail.ru>
2022-04-05 19:14:50 +0300

    Большая часть недостатков устранена. Перехожу к element и позиционированию popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,34 @@
-// Реализация функции открытия/закрытия модального окна кнопки редактирования информации о пользователе
+// Собрали все переменные в начале страницы
 
 const editingButton = document.querySelector('.profile-info__editing-button'); //Нашли кнопку
 const modalWindow = document.querySelector('.popup'); //нашли модальное окно
 const buttonCloseModalWindow = modalWindow.querySelector('.popup__close-button');//Нашли кнопку закрытия модального окна
-
-function toggleEditWindow() { //функция открытия/закрытия модального окна путём добавления класса с соотв. стилем
-  modalWindow.classList.toggle('popup_activ'); //Добавляем КЛАСС, а не селектор!!!
-}
-
-editingButton.addEventListener('click', toggleEditWindow); //добавили прослушиватель события - нажатие найденой кнопке
-
-buttonCloseModalWindow.addEventListener('click', toggleEditWindow); //добавили прослушиватель события - нажатие на "крестик" закрывания окна
-
-function onOverlayClick(event) { 
-  if (event.target === event.currentTarget) {//Условие "если объект нажатия - объект с соответствующим обработчиком события"
-    toggleEditWindow()// то запускается соответствующая функция
-  }
-}
-
-modalWindow.addEventListener('click', onOverlayClick); //добавили прослушиватель события - нажатие на оверлей (пространство вокруг модального окна)
-
-// Реализация функции редактирования данных профиля
 
 const formUserData = document.querySelector('.profile-info__data');//нашли форму с данными профиля пользователя
 const userName = document.querySelector('.profile-info__name');//нашли поле с именем пользователя
 const userActivityType = document.querySelector('.profile-info__activity-type');// нашли поле с родом деятельности пользователя
 
 const popUpFormUserData = document.querySelector('.popup__form');//в модальном окне нашли форму с данными профиля пользователя
-const popUpUserName = document.querySelector('.popup__input-name');//в модальном окне нашли поле с именем пользователя
-const popUpUserActivityType = document.querySelector('.popup__input-activity-type');//в модальном окне нашли поле с родом деятельности пользователя
+const popUpUserName = document.querySelector('.popup__input_content_name');//в модальном окне нашли поле с именем пользователя
+const popUpUserActivityType = document.querySelector('.popup__input_content_activity-type');//в модальном окне нашли поле с родом деятельности пользователя
+
+// Реализация функции открытия/закрытия модального окна кнопки редактирования информации о пользователе
+
+function toggleEditWindow() { //функция открытия/закрытия модального окна путём добавления класса с соотв. стилем
+  modalWindow.classList.toggle('popup_activ'); //Добавляем КЛАСС, а не селектор!!!
+}
+
+// Реализация функции закрытия модального окна по нажатию оверлея (закоммичена по требованию ревьюера)
+
+//function onOverlayClick(event) { 
+//  if (event.target === event.currentTarget) {//Условие "если объект нажатия - объект с соответствующим обработчиком события"
+//    toggleEditWindow()// то запускается соответствующая функция
+//  }
+//}
+
+//modalWindow.addEventListener('click', onOverlayClick); //добавили прослушиватель события - нажатие на оверлей (пространство вокруг модального окна)
+
+// Реализация функции редактирования данных профиля
 
 function openEditWindow() { //функция показывающая, что при открытии модального окна мы видим
   popUpUserName.value = userName.textContent; //что в поле "введите ваше имя" фигурируют данные ранее указанные в имени пользователя профиля
@@ -47,5 +47,11 @@ function formSubmitHandler (event) {
   
   toggleEditWindow(); //после выполнения функции редактирования данных профиля происходит закрытие модального окна
 }
+
+// Собрали все "прослушиватели событий" в конце файла
+
+editingButton.addEventListener('click', toggleEditWindow); //добавили прослушиватель события - нажатие найденой кнопке
+
+buttonCloseModalWindow.addEventListener('click', toggleEditWindow); //добавили прослушиватель события - нажатие на "крестик" закрывания окна
 
 popUpFormUserData.addEventListener('submit', formSubmitHandler); //Обработчик события, отвечающий за отправку данных профиля
